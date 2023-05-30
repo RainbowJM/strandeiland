9a6e4573353b6a1ea5f7679641731e48fbf7c1ad
Jevona Magdalena <jmagdalena@ictual.com>
2023-05-30 17:09:15 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const path = require('path');
+const io = require('socket.io')(http);
+const port = process.env.PORT || 6954;
+const bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static(path.resolve('public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+
+io.on('connection', (socket) => {
+    console.log(`user ${socket.id} connected`)
+
+    socket.emit('history', history)
+
+    socket.on('message', (message) => {
+        while (history.length > historySize) {
+            history.shift()
+        }
+        history.push(message)
+
+        io.emit('message', message)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`user ${socket.id} disconnected`)
+    })
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}.`);
+});
