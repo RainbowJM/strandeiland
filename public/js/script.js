45806bf1bfe263af345876b107fc8982fd2e8f78
Jevona Magdalena <jmagdalena@ictual.com>
2023-06-02 11:36:35 +0200

    fixed let en const

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,18 @@
 const filterMenu = document.querySelector(".filter-menu");
 const themaFilterBtn = document.querySelector(".thema-btn");
+const themeSelect = document.getElementById("thema");
+
 
 themaFilterBtn.addEventListener("click", () => {
   filterMenu.classList.toggle("show-filter");
 });
 
-function displaySelectedOption(selectElement) {
-  var selectedOption = selectElement.options[selectElement.selectedIndex].text;
-  selectElement.value = selectedOption;
-}
-
-var themeSelect = document.getElementById("thema");
 themeSelect.addEventListener("change", function () {
   displaySelectedOption(themeSelect);
 });
+
+
+function displaySelectedOption(selectElement) {
+  let selectedOption = selectElement.options[selectElement.selectedIndex].text;
+  selectElement.value = selectedOption;
+}
