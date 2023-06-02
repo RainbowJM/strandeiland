7bdf0c08e4491e98351976168cbc388518894f1f
Laiba Choudhry <laiba@hotmail.nl>
2023-06-02 06:26:25 +0200

    AANPASSUH

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,12 @@
 const socket = io();
+
+function displaySelectedOption(selectElement) {
+    var selectedOption = selectElement.options[selectElement.selectedIndex].text;
+    selectElement.value = selectedOption;
+  }
+  
+  var themeSelect = document.getElementById("thema");
+  themeSelect.addEventListener("change", function() {
+    displaySelectedOption(themeSelect);
+  });
+  
