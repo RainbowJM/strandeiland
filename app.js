daf376ed83447c05efb968f9d3fdf92b137398cc
Jevona Magdalena <jmagdalena@ictual.com>
2023-06-02 11:19:50 +0200

    dutch -> english

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const path = require('path');
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const path = require("path");
+const io = require("socket.io")(http);
 const port = process.env.PORT || 6954;
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use(express.static(path.resolve('public')));
+app.use(express.static(path.resolve("public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.render('index',{
-        title: 'Wensen'
-    });
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Wensen",
+  });
 });
 
-
-app.get('/formulier', (req, res) => {
-    res.render('formulier');
+app.get("/form", (req, res) => {
+  res.render("form", {
+    title: "Formulier",
+  });
 });
 
+io.on("connection", (socket) => {
+  console.log(`user ${socket.id} connected`);
 
-io.on('connection', (socket) => {
-    console.log(`user ${socket.id} connected`)
+  socket.emit("history", history);
 
-    socket.emit('history', history)
+  socket.on("message", (message) => {
+    while (history.length > historySize) {
+      history.shift();
+    }
+    history.push(message);
 
-    socket.on('message', (message) => {
-        while (history.length > historySize) {
-            history.shift()
-        }
-        history.push(message)
+    io.emit("message", message);
+  });
 
-        io.emit('message', message)
-    })
-
-    socket.on('disconnect', () => {
-        console.log(`user ${socket.id} disconnected`)
-    })
+  socket.on("disconnect", () => {
+    console.log(`user ${socket.id} disconnected`);
+  });
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on  http://localhost:${port}`);
+  console.log(`Example app listening on  http://localhost:${port}`);
 });
