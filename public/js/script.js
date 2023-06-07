9024095f97fa757122ec634ac40a6316a72cc76f
K3A101 <keisha.alexander@hva.nl>
2023-06-07 12:44:35 +0200

    filter css style

--- public/js/script.js
+++ public/js/script.js
@@ -2,7 +2,7 @@ const tabs = document.querySelectorAll('[data-filter-target]');
 const tabContents = document.querySelectorAll('[data-filter-content]');
 const filterThemeBtn = document.querySelector('.filter-theme-btn');
 const filterSortBtn = document.querySelector('.filter-sort-btn');
-
+const closeFilterBtn = document.querySelector('.close-filter')
 
 console.log('tabs', tabs)
 const themeSelect = document.getElementById("thema");
@@ -13,7 +13,8 @@ function toggleFilterMenu(){
 
   tabs.forEach(tab => {
     const target = document.querySelector(tab.dataset.filterTarget);
-    tab.addEventListener('click', () => {;
+    tab.addEventListener('click', (e) => {
+      e.preventDefault();
    tabContents.forEach(tabContent => tabContent.classList.remove('active'));
    target.classList.add('active');
     })
@@ -21,15 +22,25 @@ function toggleFilterMenu(){
     filterThemeBtn.addEventListener('click', (e) => {
      e.preventDefault();
       target.classList.remove('active');
+      console.log('form submitted')
+
     }
     )
     filterSortBtn.addEventListener('click', (e) => {
       e.preventDefault();
       target.classList.remove('active');
       console.log('form submitted')
-    }
-    )
+    })
+
+    
+    closeFilterBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      target.classList.remove('active');
+      console.log('form submitted')
+    })
   })
+
+
 }
 
 
