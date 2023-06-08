da7f56454a3ff54d26ba08723474b3b0834e372c
K3A101 <keisha.alexander@hva.nl>
2023-06-08 18:13:39 +0200

    preview selected filter items started

--- public/js/script.js
+++ public/js/script.js
@@ -45,6 +45,7 @@ if (themeSelect) {
 
 // filter menu
 toggleFilterMenu();
+// previewFilterItems();
 
 
 // ------------------ functions -------------------------------------------------------
@@ -91,3 +92,17 @@ function toggleFilterMenu(){
     })
   })
 }
+
+
+// function previewFilterItems(){  
+//   console.log('filter items');
+//   const filterItems = document.querySelectorAll('.theme-filter-items li, .sorting-filter-items li');
+//   let selectedFilterItems = [];
+//   console.log('filter items', filterItems);
+//   filterItems.forEach(filterItem => { 
+//     filterItem.addEventListener('click', (e) => {
+
+//       e.preventDefault();
+//     })
+//   })
+// }
