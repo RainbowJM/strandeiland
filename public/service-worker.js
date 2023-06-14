6f7820943a96dffd8244b22dc04d9cc4b9c4f744
Jevona Magdalena <jmagdalena@ictual.com>
2023-06-14 11:46:57 +0200

    fetch done

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,6 +9,7 @@ const assets = [
     '/css/overview-page.css',
     '/css/sent.css',
     '/img/favicon.ico',
+    '/offline',
 ];
 
 
@@ -36,3 +37,49 @@ self.addEventListener('activate', event => {
     )
 });
 
+self.addEventListener('fetch', event => {
+    console.log('Fetch event: ', event.request.url);
+    if (isCoreGetRequest(event.request)) {
+        event.respondWith(
+            caches.open(cacheName)
+                .then(cache => cache.match(event.request.url))
+        );
+    } else if (isHtmlGetRequest(event.request)) {
+        event.respondWith(
+            caches.open(runtimeCacheName)
+                .then(cache => cache.match(event.request.url))
+                .then(response => response ? response : fetchAndCache(event.request, runtimeCacheName))
+                .catch(e => {
+                    return caches.open(cacheName)
+                        .then(cache => cache.match('/offline'))
+                })
+        );
+    }
+});
+
+function isCoreGetRequest(request) {
+    return request.method === 'GET' && assets.includes(getPathName(request.url));
+}
+
+function isHtmlGetRequest(request) {
+    return request.method === 'GET' && 
+    (request.headers.get('accept') !== null && request.headers.get('accept').indexOf('text/html') > -1);
+}
+
+function fetchAndCache(request, cacheName) {
+    return fetch(request)
+        .then(response => {
+            if (!response.ok) {
+                throw new TypeError('Bad response status');
+            }
+            const clone = response.clone();
+            caches.open(cacheName).then((cache) => cache.put(request, clone));
+            limitCacheSize(cacheName,5);
+            return response;
+        })
+}
+
+function getPathName(requestUrl) {
+    const url = new URL(requestUrl);
+    return url.pathname;
+}
