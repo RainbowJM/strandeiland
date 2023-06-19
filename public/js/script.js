9051d80a60dc817c129b79ec596693c53f3dcaca
Jevona Magdalena <jmagdalena@ictual.com>
2023-06-19 13:24:58 +0200

    Fixed merge conflict

--- public/js/script.js
+++ public/js/script.js
@@ -1,37 +1,34 @@
+// ------------------ variables -------------------------------------------------------
+import { messages, submitMessage, input, tabs, filterMenu, themeFilterBtn, themeSelect, asideItems, theMenuButton, typingElement, selectedOption, dropdownMenu, localStorageKey, themeCheckboxes, fileInput, 
+  customImagePreview, selectedFileName, popupCloseButton, popup, savedFormData, imageLinkInput, imagePreview, filterThemeBtn, uploadButton} from "./modules/variables.js";
+import { toggleFilterMenu } from "./modules/filter.js";
+import { toggleMenu } from "./modules/navigationMenu.js";
 
-const selectedOption = document.querySelector(".selected-option");
-const dropdownMenu = document.getElementById("themeDropdownMenu");
-const localStorageKey = "formData";
-const themeCheckboxes = document.querySelectorAll("#themeDropdownMenu input[type='checkbox']");
-const fileInput = document.getElementById("file");
-const customImagePreview = document.getElementById("customImagePreview");
-const selectedFileName = document.getElementById("selectedFileName");
-const closeButton = document.getElementById("closeButton");
-const popupCloseButton = document.getElementById("popupCloseButton");
-const popup = document.getElementById("popup");
+const socket = io();
+let last;
 
-
-
+// ------------------ logic -------------------------------------------------------
+if (uploadButton) {
 uploadButton.addEventListener("click", handleUploadButtonClick);
-
+}
 // Event listener for closeButton click
-popupCloseButton.addEventListener("click", handlePopupCloseButtonClick);
-
-
-
-
-
+if (popupCloseButton) {
+  popupCloseButton.addEventListener("click", handlePopupCloseButtonClick);
+}
 
 // Event listener for selectedOption click
+if (selectedOption) {
 selectedOption.addEventListener("click", handleSelectedOptionClick);
+}
 
 // Event listener for themeCheckboxes change
+if (themeCheckboxes) {
 themeCheckboxes.forEach(function(checkbox) {
   checkbox.addEventListener("change", handleCheckboxChange);
 });
+}
 
 // Retrieve form data from localStorage
-const savedFormData = localStorage.getItem(localStorageKey);
 if (savedFormData) {
   const parsedFormData = JSON.parse(savedFormData);
 
@@ -48,138 +45,53 @@ if (savedFormData) {
   updateFormData();
 }
 
-
-
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  const imageLinkInput = document.getElementById('imageLink');
-
-  const imagePreview = document.getElementById('imagePreview');
-  imageLinkInput.addEventListener('input', () => {
-      imagePreview.innerHTML = `<img src="${imageLinkInput.value}" alt="">`;
-
-
- 
-
-  });
-
+if (imageLinkInput) {
+imageLinkInput.addEventListener('input', () => {
+  imagePreview.innerHTML = `<img src="${imageLinkInput.value}" alt="">`;
 });
+}
 
-fileInput.addEventListener("change", function() {
-  const file = fileInput.files[0];
 
-  if (file) {
-    selectedFileName.textContent = file.name;
-
-    const reader = new FileReader();
-    reader.onload = function(e) {
-      const img = document.createElement("img");
-      img.src = e.target.result;
-      img.alt = "Selected Image";
-      customImagePreview.innerHTML = "";
-
-      const closeButton = document.createElement("button");
-      closeButton.classList.add("close-button");
-      closeButton.innerHTML = "&times;"; // Use × symbol as the close icon
-
-      closeButton.addEventListener("click", function() {
-        // Clear the file input value
-        fileInput.value = "";
-
-        // Clear the image preview
+if (fileInput) {
+  fileInput.addEventListener("change", function() {
+    const file = fileInput.files[0];
+  
+    if (file) {
+      selectedFileName.textContent = file.name;
+  
+      const reader = new FileReader();
+      reader.onload = function(e) {
+        const img = document.createElement("img");
+        img.src = e.target.result;
+        img.alt = "Selected Image";
         customImagePreview.innerHTML = "";
-        selectedFileName.textContent = "Geen bestand geselecteerd";
-
-        // Remove the close button
-        closeButton.remove();
-      });
-
-      customImagePreview.appendChild(img);
-      customImagePreview.appendChild(closeButton);
-    };
-    reader.readAsDataURL(file);
-  } else {
-    selectedFileName.textContent = "Geen bestand geselecteerd";
-    customImagePreview.innerHTML = "";
-
-// ------------------ variables -------------------------------------------------------
-import {messages,submitMessage,input,tabs,filterMenu,themeFilterBtn,themeSelect,asideItems,theMenuButton,typingElement} from "./modules/variables.js";
-import { toggleFilterMenu } from "./modules/filter.js";
-import { toggleMenu } from "./modules/navigationMenu.js";
-
-const socket = io();
-let last;
-
-console.log("tabs", tabs);
-console.log("themeSelect", themeSelect);
-
-// ------------------ logic -------------------------------------------------------
-document.addEventListener("DOMContentLoaded", function () {
-  const selectedOption = document.querySelector(".selected-option");
-  const dropdownMenu = document.getElementById("themeDropdownMenu");
-  const localStorageKey = "formData";
-  console.log("selectedOption", selectedOption);
-
-  if (selectedOption) {
-    selectedOption.addEventListener("click", function () {
-      dropdownMenu.classList.toggle("show");
-    });
-  }
-
-  function updateFormData() {
-    const checkboxes = document.querySelectorAll(
-      "#themeDropdownMenu input[type='checkbox']"
-    );
-    const selectedThemes = [];
-    checkboxes.forEach(function (checkbox) {
-      if (checkbox.checked) {
-        selectedThemes.push(checkbox.value);
-      }
-    });
-    selectedOption.textContent =
-      selectedThemes.length > 0
-        ? selectedThemes.join(", ")
-        : "Selecteer de passende thema's";
-
-    const formData = {
-      titel: document.getElementById("titel").value,
-      beschrijving: document.getElementById("beschrijving").value,
-      themas: selectedThemes,
-    };
-
-    // Save form data to localStorage
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
-  }
-
-  const themeCheckboxes = document.querySelectorAll(
-    "#themeDropdownMenu input[type='checkbox']"
-  );
-  themeCheckboxes.forEach(function (checkbox) {
-    checkbox.addEventListener("change", updateFormData);
+  
+        const closeButton = document.createElement("button");
+        closeButton.classList.add("close-button");
+        closeButton.innerHTML = "&times;"; // Use × symbol as the close icon
+  
+        closeButton.addEventListener("click", function() {
+          // Clear the file input value
+          fileInput.value = "";
+  
+          // Clear the image preview
+          customImagePreview.innerHTML = "";
+          selectedFileName.textContent = "Geen bestand geselecteerd";
+  
+          // Remove the close button
+          closeButton.remove();
+        });
+  
+        customImagePreview.appendChild(img);
+        customImagePreview.appendChild(closeButton);
+      };
+      reader.readAsDataURL(file);
+    } else {
+      selectedFileName.textContent = "Geen bestand geselecteerd";
+      customImagePreview.innerHTML = "";
+    }
   });
-
-  // Retrieve form data from localStorage
-  const savedFormData = localStorage.getItem(localStorageKey);
-  if (savedFormData) {
-    const parsedFormData = JSON.parse(savedFormData);
-
-    // Set the saved values in the corresponding form fields
-    document.getElementById("titel").value = parsedFormData.titel;
-    document.getElementById("beschrijving").value = parsedFormData.beschrijving;
-
-    parsedFormData.themas.forEach(function (theme) {
-      const checkbox = document.querySelector(
-        `#themeDropdownMenu input[type='checkbox'][value='${theme}']`
-      );
-      if (checkbox) {
-        checkbox.checked = true;
-      }
-    });
-    updateFormData();
-
-  }
-});
+}
 
 if (asideItems.length > 0) {
   asideItems.forEach((asideItem) => {
@@ -190,11 +102,13 @@ if (asideItems.length > 0) {
   });
 }
 
-if (tabs) {
+if (filterThemeBtn) {
   toggleFilterMenu();
+  console.log("filterThemeBtn");
 }
 
 if (submitMessage) {
+  console.log("submitMessage");
   submitMessage.addEventListener("click", (event) => {
     event.preventDefault();
 
@@ -254,8 +168,6 @@ if (themeSelect) {
   });
 }
 
-// filter menu
-
 if (theMenuButton) {
   theMenuButton.addEventListener("click", toggleMenu);
 }
@@ -312,7 +224,8 @@ function handleUploadButtonClick() {
 
    
 // Function to handle closeButton click
-function handlePopupCloseButtonClick() {
+function handlePopupCloseButtonClick(event) {
+  event.preventDefault(); // Prevent the default form submission behavior
   popup.classList.add("hidden");
 }
 
@@ -321,7 +234,8 @@ function handlePopupCloseButtonClick() {
 function handleCheckboxChange() {
   updateFormData();
 }
-    function displaySelectedOption(selectElement) {
+
+function displaySelectedOption(selectElement) {
   let selectedOption = selectElement.options[selectElement.selectedIndex].text;
   selectElement.value = selectedOption;
 }
@@ -338,8 +252,12 @@ function add(message, name, time, id) {
       </div>
       </section>`,
     })
+  )
+  messages.scrollTop = messages.scrollHeight;
+  last = id;
+};
     
-    // Function to update form data
+// Function to update form data
 function updateFormData() {
   const checkboxes = document.querySelectorAll("#themeDropdownMenu input[type='checkbox']");
   const selectedThemes = [];
@@ -362,32 +280,4 @@ function updateFormData() {
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
 }
 
-  
-    filterThemeBtn.addEventListener('click', (e) => {
-     e.preventDefault();
-      target.classList.remove('active');
-      console.log('form submitted')
-
-    }
-    )
-    filterSortBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      target.classList.remove('active');
-      console.log('form submitted')
-    })
-
-    
-    closeFilterBtn.addEventListener('click', (e) => {
-      e.preventDefault();
-      target.classList.remove('active');
-      console.log('form submitted')
-    })
-  })
-}
-
-  );
-  // Scroll to the bottom of the chat.
-  messages.scrollTop = messages.scrollHeight;
-  last = id;
-}
 
