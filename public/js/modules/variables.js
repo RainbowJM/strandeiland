f3acac96d5cc8d142138be46e90e625e0a3f174e
K3A101 <keisha.alexander@hva.nl>
2023-06-25 00:01:04 +0200

    dynamic icon on the user page

--- public/js/modules/variables.js
+++ public/js/modules/variables.js
@@ -40,9 +40,9 @@ const imageLinkInput = document.getElementById("imageLink");
 const imagePreview = document.getElementById("imagePreview");
 const uploadButton = document.getElementById("uploadButton");
 const boxes = document.querySelectorAll(".box-click");
-const ambassadorsIconValue = document.querySelectorAll(".thumbnail-container .statistics li:nth-child(1) span");
-const helperIconValue = document.querySelectorAll(".thumbnail-container .statistics li:nth-child(2) span ");
-const votersIconValue = document.querySelectorAll(".thumbnail-container .statistics li:nth-child(3) span ");
+const ambassadorsIconValue = document.querySelectorAll(" .statistics li:nth-child(1) span");
+const helperIconValue = document.querySelectorAll(" .statistics li:nth-child(2) span ");
+const votersIconValue = document.querySelectorAll(".statistics li:nth-child(3) span ");
 
 const titleInput = document.getElementById("title");
 const descriptionTextarea = document.getElementById("description");
